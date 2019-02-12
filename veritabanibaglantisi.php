648fe027dfc840428a2c969b85cf64e6c9e71bbb
Elif Özceylan <elifozceylan01@outlook.com>
2019-02-13 00:49:07 +0300

    Update veritabanibaglantisi.php

--- veritabanibaglantisi.php
+++ veritabanibaglantisi.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 $servername = "localhost";
-$username = "elif";
-$password = "123456";
+$username = "";
+$password = "";
 $dbadi="hotel";
  try{
     $baglanti = new PDO("mysql:host=$servername;dbname=$dbadi;charset=utf8", $username, $password);
@@ -17,20 +17,3 @@ catch(PDOException $e)
 }
 
 ?>
-<?php
-/*if(!isset($_REQUEST['dil']))
-
-        {
-        $dil="tr";
-        $dosya=$dil.".php";
-        $_SESSION['dil']=$dosya;
-        include($dosya);
-        }
-
-      else{
-            $dil=$_REQUEST['dil'];
-           $dosya=$dil.".php";
-           $_SESSION['dil']=$dosya;
-          include($dosya);
-      }*/
-    ?>
